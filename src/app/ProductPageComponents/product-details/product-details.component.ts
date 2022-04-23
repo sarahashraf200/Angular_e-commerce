da2e69f442dcd35deb570ea0e12a3ccdda7b18fe
sarah ashraf <sarahashraf200@gmail.com>
2022-04-23 13:46:01 +0200

    change in cart page style

--- src/app/ProductPageComponents/product-details/product-details.component.ts
+++ src/app/ProductPageComponents/product-details/product-details.component.ts
@@ -13,6 +13,8 @@ export class ProductDetailsComponent implements OnInit {
  size_selection = "";
  color_selection = "";
  qt : number = 0;
+ total: number = 0;
+ sum : number =0;
   
   constructor (public dservice: ProductDataService) {
   
@@ -20,8 +22,15 @@ export class ProductDetailsComponent implements OnInit {
    }
 
    Submit (){
+     if (this.qt !=0){
+     this.total = this.price * this.qt;}
+     else {
+       this.total = this.price;
+       this. qt = 1;
+     }
+     this.sum = this.sum + this.price;
      var product = {name : this.name , price : this.price , desc : this.desc ,size : this.size_selection , color : this.color_selection,
-      qt: this.qt }
+      qt: this.qt , total: this.total , sum: this.sum}
      this.dservice.setProduct(product);
    
    }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,7 +7,7 @@ import { BehaviorSubject , observable} from 'rxjs';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  public car_y : any= 0;
+ /* public car_y : any= 0;
   public car_y1  : any = 0;
   public car_y2 : any= 0;
   public total1 : any= 594;
@@ -20,20 +20,29 @@ export class CartComponent implements OnInit {
      desc: '',
      price : 0
 
-   }
+   }*/
   data:Array<any> = [ 1 , 2 , 3 , 4 ,5 ];
-  
   public products : any =[];
   constructor(public cartService: ProductDataService) { }
-  
+   sum : number = 0;
+   ship : number = 10;
   ngOnInit(): void {
+    this.products = []
    this.cartService.getProducts()
    .subscribe(res=>{
     console.log(res)
     // var item = {name : res.name , price : res.price , desc : res.desc ,size : res.size_selection , color : res.color_selection,
     //   qt: res.qt }
       this.products.push(res)
+      console.log(this.products)
+      this.products.forEach((item : any) => {
+        this.sum += item.sum;
+      
+      });
+      console.log(this.sum);
    })
+   
+ 
  
   }
   
