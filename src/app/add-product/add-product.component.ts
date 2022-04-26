365423cbfec65b683272aa25b021892cc376822b
mennaahisham <mennaahisham99@yahoo.com>
2022-04-26 07:56:39 +0200

    AddProduct changes

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductDataService } from '../product-data.service' ; 
 
 @Component({
   selector: 'app-add-product',
@@ -9,9 +10,24 @@ export class AddProductComponent implements OnInit {
 
   one = ['jackets']
   two=['shirts']
+  name: string = "Lightweight Jacket";
+  price : number = 50;
+  desc : string = " Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat."
+  size_selection = "";
+  color_selection = "";
+  qt : number = 0;
+  total: number = 0;
+  sum : number =0;
 
-  constructor() { }
-
+  constructor(public dservice: ProductDataService) { }
+  Submit (){
+   
+    //this.sum = this.sum + this.price;
+    var product = {name : this.name , price : this.price , desc : this.desc ,size : this.size_selection , color : this.color_selection,
+     qt: this.qt , total: this.total , sum: this.sum}
+    this.dservice.setProduct(product);
+  
+  }
   ngOnInit(): void {
   }
 
