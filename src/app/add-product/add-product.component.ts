55b6f30b4bb48a4dde074003b3249031879d9956
mennaahisham <mennaahisham99@yahoo.com>
2022-04-27 09:45:55 +0200

    add-product updated

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductCRUDService } from 'src/app/ProductPageComponents/CRUD/product-crud.service';
+import { ProductInfo } from '../ProductPageComponents/CRUD/ProductInfo';
+
 
 @Component({
   selector: 'app-add-product',
@@ -10,9 +13,28 @@ export class AddProductComponent implements OnInit {
   one = ['jackets']
   two=['shirts']
 
-  constructor() { }
 
+  price=""
+  name=""
+  desc=""
+  img= ""
+
+    constructor(private productCrudApi: ProductCRUDService,  public productService: ProductDataService) {
+      
+     }
+ 
   ngOnInit(): void {
-  }
+   
+     };
+
+onSubmit(){
+    this.productCrudApi.AddProduct(data).valueChanges().subscribe(data=>{
+        this.name = data['name']
+        this.price = data['price']
+        this.desc = data['desc']
+        this.img = data['img']   
+        })
+    ;
+}
 
 }
