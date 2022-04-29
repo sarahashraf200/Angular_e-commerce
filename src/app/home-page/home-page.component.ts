48fdc4bdc4e1a49ff385cae80c2c23d4039f78bd
Moaaz <moaaz5799@gmail.com>
2022-04-29 03:10:21 +0200

    adding component logic for home and adding id in the database

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductDataService } from '../product-data.service';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth.service';
+import { ProductCRUDService } from '../ProductPageComponents/CRUD/product-crud.service';
 
 @Component({
   selector: 'app-home-page',
@@ -9,17 +10,31 @@ import { AuthService } from 'src/app/shared/auth.service';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
+  Products :Array<any> = [];
   selectedProduct : any
   itemId:string = "Jacket"
   constructor(private dataService: ProductDataService,
-    private route: ActivatedRoute , private auth : AuthService) { }
+    private route: ActivatedRoute , private auth : AuthService, private crud: ProductCRUDService) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
   logout (){
     this.auth.logout();
   }
+  getProducts(){
+    this.crud.GetProductsList().valueChanges().subscribe(data => {
+      if (data != undefined) {
+        this.Products = data;
+        // console.log(this.Products);
+        console.log(this.Products[0].desc);
+        // console.log(this.Products[0][0]);
+        
+      }
+    }
+    );
+  }
+
 
 
 }
